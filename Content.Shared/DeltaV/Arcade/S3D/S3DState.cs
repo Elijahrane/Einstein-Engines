a598f7f220c5e7c61832f9f3242696b492c92a3c
Rane <elijahrane@gmail.com>
2023-12-16 16:28:00 -0500

    add input bitflags to state, verify it can be serialized properly in-game

--- Content.Shared/DeltaV/Arcade/S3D/S3DState.cs
+++ Content.Shared/DeltaV/Arcade/S3D/S3DState.cs
@@ -11,8 +11,8 @@ public struct S3DState
     public double DirY;
     public double PlaneX;
     public double PlaneY;
-
-    public S3DState(double posX, double posY, double dirX, double dirY, double planeX, double planeY)
+    public InputFlags Input;
+    public S3DState(double posX, double posY, double dirX, double dirY, double planeX, double planeY, InputFlags input)
     {
         PosX = posX;
         PosY = posY;
@@ -20,5 +20,20 @@ public struct S3DState
         DirY = dirY;
         PlaneX = planeX;
         PlaneY = planeY;
+        Input = input;
     }
+}
+
+/// <summary>
+/// The arcade keys pressed down.
+/// </summary>
+[Flags]
+public enum InputFlags : byte
+{
+    None = 0,
+    Up = 1,
+    Down = 1 << 1,
+    Left = 1 << 2,
+    Right = 1 << 3,
+    Fire = 1 << 4
 }
--- Content.Shared/DeltaV/Arcade/S3D/SharedS3DArcadeSystem.cs
+++ Content.Shared/DeltaV/Arcade/S3D/SharedS3DArcadeSystem.cs
@@ -10,6 +10,6 @@ public abstract class SharedS3DArcadeSystem : EntitySystem
 
     private void OnInit(EntityUid uid, S3DArcadeComponent component, ComponentInit args)
     {
-        component.State = new S3DState(22, 12, -1, 0, 0, 0.66);
+        component.State = new S3DState(22, 12, -1, 0, 0, 0.66, InputFlags.None);
     }
 }
